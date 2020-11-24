1170e28aba15d7f81b501e4aa83de82a7ff7cc1a
a1005002587 <38067318+a1005002587@users.noreply.github.com>
2020-11-24 17:31:19 +0800

    Update C189Checkin.py

--- C189Checkin.py
+++ C189Checkin.py
@@ -7,7 +7,7 @@ username = ""
 password = ""
 
 
-#Server酱报错推送提醒，需要填下下面的key，官网：https://sc.ftqq.com/3.version
+#Server酱报错推送提醒，需要填下下面的key，官网：https://sc.ftqq.com/3.version。。。
 SCKEY = ""
 #推送url
 scurl = f"https://sc.ftqq.com/{SCKEY}.send"
